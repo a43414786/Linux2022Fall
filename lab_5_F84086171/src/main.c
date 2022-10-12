43019a0c6160b5c5d52904e234868d3283612831
a43414786 <a43414786@gmail.com>
2022-10-12 12:27:14 +0800

    k

---
+++ lab_5_F84086171/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/strcpy.h"
+int mian(){
+    const char *src = "f84086171";
+    char *dest = malloc(10);
+    dest = sstrcpy(dest,src);
+    printf("%s\n",dest);
+    return 0;
+}
---
+++ lab_5_F84086171/src/strcpy.c
@@ -0,0 +1,5 @@
+#include "../include/strcpy.h"
+
+char *sstrcpy(char *dest,const char *src){
+    return 0;
+}
